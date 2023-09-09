51623251987da6cf031c869ae5bb3c01141e0cad
jalentwright <jalen_t_wright@yahoo.com>
2023-09-09 04:32:35 -0500

    n/a

--- javascript.js
+++ javascript.js
@@ -1,83 +1,74 @@
-// const options = ["rock", "paper", "scissors"];
-
-// function getComputerChoice(){
-//     const choice = options[Math.floor(Math.random() * options.length)];
-//     return choice;
-// }
-
-// function checkWinner(playerSelection, computerSelection){
-//     if(playerSelection == computerSelection){
-//         return "tie";
-//     }
-//     else if (
-//         (playerSelection == "rock" && computerSelection == "scissors") ||
-//         (playerSelection == "scissors" && computerSelection == "paper") ||
-//         (playerSelection == "paper" && computerSelection == "rock")
-//     ){
-//         return "Player";
-//     }
-//     else {
-//         return "computer";
-//     }
-// }
-
-// function playRound(playerSelection, computerSelection){
-//     const result = checkWinner(playerSelection, computerSelection)
-//     if(result == "tie"){
-//         return "It's a Tie!"
-//     }
-//     else if(result=="Player"){
-//         return `You Win! ${playerSelection} beats ${computerSelection}`
-//     }
-//     else{
-//         return `You Lose! ${computerSelection} beats ${playerSelection}`
-//     }
-// }
-
-// function getPlayerChoice(){
-//     let validatedInput = false;
-//     while(validatedInput == false){
-//         const choice = prompt("Rock Paper Scissors")
-//         if(choice == null){
-//             continue;
-//         }
-//         const choiceInLower = choice.toLowerCase();
-//         if(options.includes(choiceInLower)){
-//         validatedInput = true;
-//         return choiceInLower;   
-//         }
-//     }
-// }
-
-// function game() {
-//     let scorePlayer = 0;
-//     let scoreComputer = 0;
-//     console.log("Welcome!")
-//     for (let i = 0; i < 5; i++) {
-//         const playerSelection = getPlayerChoice();
-//         const computerSelection = getComputerChoice();
-//         console.log(playRound(playerSelection, computerSelection));
-//         console.log("-------------");
-//         if(checkWinner(playerSelection, computerSelection) == "Player"){
-//             scorePlayer++;
-//         } 
-//         else if(checkWinner(playerSelection, computerSelection) == "Computer"){
-//             scoreComputer++;
-//         }
-
-//     }
-//     console.log("GAME OVER!!")
-//     if(scorePlayer > scoreComputer){
-//         console.log("player was the winner!");
-//     }
-//     else if(scorePlayer < scoreComputer){
-//         console.log("Computer wins!!!");
-//     }
-//     else{
-//         console.log("We have a tie :/ ");
-//     }
-// }
-
-// game()
+let userScore =  0;
+let computerScore =   0;
+let tieScore = 0;
+const userScoreClass = document.getElementById("player-score");
+const computerScoreClass = document.getElementById("computer-score");
+const tieScoreClass = document.getElementById("tie-score");
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
+const result = document.querySelector('.result > p');
 
 
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return options[randomNumber];
+}
+
+function getPlayerChoice() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+}
+
+getPlayerChoice();
+
+function win(player, computer) {
+    userScore++;
+    userScoreClass.innerHTML = userScore;
+    computerScoreClass.innerHTML = computerScore;
+    result.innerHTML = `NICE! ${player} beats ${computer} you win... This time :>`; 
+}
+
+function lose(player, computer) {
+    computerScore++;
+    computerScoreClass.innerHTML = computerScore;
+    result.innerHTML = `OH NO! ${computer} beats ${player} you lose :(`
+}
+
+function tie(player, computer) {
+    tieScore++;
+    tieScoreClass.innerHTML = tieScore;
+    result.innerHTML = `hm thats weird a draw.... try again!`
+}
+
+function game(playerChoice) {
+    const computerChoice = getComputerChoice();
+        switch(playerChoice + computerChoice) {
+            case "rockscissors" :
+            case "paperrock":
+            case "scissorspaper":
+                win(playerChoice, computerChoice);
+                break;
+            case "rockpaper":
+            case "paperscissors":
+            case "scissorsrock":
+                lose(playerChoice, computerChoice)
+                break;
+            case "rockrock":
+            case "paperpaper":
+            case "scissorsscissors":
+                tie(playerChoice, computerChoice)
+                break;
+        
+        }
+}
